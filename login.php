2e5fa58c0a37c7bf3cd02f2d6b166c5f034a04ab
haleema2001 <haleema.b2022@gmail.com>
2026-03-12 00:49:10 -0400

    Create login.php
    Added login page (basic PHP form)

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login - Horizon Realty</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<h1>Login</h1>
+
+<form method="POST">
+    <label>Email</label><br>
+    <input type="email" name="email"><br><br>
+
+    <label>Password</label><br>
+    <input type="password" name="password"><br><br>
+
+    <button type="submit">Login</button>
+</form>
+
+</body>
+</html> 
